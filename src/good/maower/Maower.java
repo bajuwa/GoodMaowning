3f4a4e763b270a7c167ba553cedb32feddd562f5
bajuwa <justcallmebaj@gmail.com>
2015-03-21 12:56:38 -0700

    Added logging capabilities that output to the file log/log.out

--- src/good/maower/Maower.java
+++ src/good/maower/Maower.java
@@ -1,10 +1,14 @@
 package good.maower;
 
+import org.apache.log4j.Logger;
+
 /**
  * Sends emails to subscribers
  */
 public class Maower {
+	static Logger logger = Logger.getLogger(Maower.class);
+
 	public static void main(String[] args) {
-		System.out.println("Good Maowning!");
+		logger.info("Good Maowning!");
 	}
 }
