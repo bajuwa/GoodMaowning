dc00a7eb70b8e70ecc50d71b690842d000b85a1a
bajuwa <justcallmebaj@gmail.com>
2015-03-22 16:30:45 -0700

    Created a simple images db (with creation script) to store cat urls

---
+++ develop/create_images_db.sql
@@ -0,0 +1,12 @@
+-- Call this script using the command: sqlite3 /data/images.db < create_images_db.sql
+
+PRAGMA foreign_keys = ON;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE images (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	url TEXT UNIQUE NOT NULL
+);
+
+COMMIT;
