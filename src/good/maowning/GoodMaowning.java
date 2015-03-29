97ea7f3d99962052db015503410de8f3f6acbb04
bajuwa <justcallmebaj@gmail.com>
2015-03-29 15:02:09 -0700

    Separated the default sleep ms time in to a multiplication equation for 5 minutes for easier readability

--- src/good/maowning/GoodMaowning.java
+++ src/good/maowning/GoodMaowning.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class GoodMaowning {
 	private static final Logger logger = Logger.getLogger(GoodMaowning.class);
 	
-	private static final int DEFAULT_SLEEP_MILLISECONDS = 60000;
+	private static final int DEFAULT_SLEEP_MILLISECONDS = 5 * 60 * 1000;
 	
 	public static void main(String[] args) throws InterruptedException {
 		/* This program will run continuously until killed */
