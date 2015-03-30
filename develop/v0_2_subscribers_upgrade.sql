3972fef695bdeff2543d2340aa9567d2cb79bb81
bajuwa <justcallmebaj@gmail.com>
2015-03-29 20:05:24 -0700

    Updated incorrect column type for last_day_sent

--- develop/v0_2_subscribers_upgrade.sql
+++ develop/v0_2_subscribers_upgrade.sql
@@ -2,4 +2,4 @@ ALTER TABLE subscribers
 ADD COLUMN send_time TEXT NOT NULL DEFAULT '09:00';
 
 ALTER TABLE subscribers
-ADD COLUMN last_date_sent TEXT;
+ADD COLUMN last_date_sent INTEGER NOT NULL DEFAULT 0;
