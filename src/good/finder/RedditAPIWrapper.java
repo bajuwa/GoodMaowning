641a6950a2fe811e1f6547e57274d89065c782a8
bajuwa <justcallmebaj@gmail.com>
2015-03-31 21:09:02 -0700

    Added http get request functionality to get information from reddit

--- src/good/finder/RedditAPIWrapper.java
+++ src/good/finder/RedditAPIWrapper.java
@@ -3,6 +3,13 @@ package good.finder;
 import org.apache.log4j.Logger;
 
 import java.util.*;
+ 
+/* HTTP Imports */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class RedditAPIWrapper {
 	private final static Logger logger = Logger.getLogger(RedditAPIWrapper.class);
@@ -39,12 +46,41 @@ public class RedditAPIWrapper {
 	}
 
 	public static List<String> getSubmissions(Subreddit sub, SubCategory category, Timespan time, int numOfEntries) {
-		/* TODO */
-		logger.debug(formUrl(sub, category, time, numOfEntries));
+		try {
+			String response = sendHttpGet(formUrl(sub, category, time, numOfEntries));
+		} catch (IOException e) {
+			logger.error(e);
+			return new ArrayList<String>();
+		}
+		
+		/* TODO: Parse into more usable form? */
+		
 		return new ArrayList<String>();
 	}
 	
 	private static String formUrl(Subreddit sub, SubCategory category, Timespan time, int numOfEntries) {
-		return String.format("https://www.reddit.com/r/%s/%s.json?t=%s&limit=%d", sub, category, time, numOfEntries);
+		return String.format("http://www.reddit.com/r/%s/%s.json?t=%s&limit=%d", sub, category, time, numOfEntries);
+	}
+	
+	private static String sendHttpGet(String url) throws IOException {
+		logger.debug("Send HTTP Get request to: " + url);
+	
+		/* Setup connection to our url */
+		URL urlObject = new URL(url);
+		HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
+		
+		/* Get Response via Stream */
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String line;
+		StringBuffer response = new StringBuffer();
+		
+		/* Read response into string buffer */
+		while ((line = reader.readLine()) != null) {
+			response.append(line);
+		}
+		
+		String finalResponse = response.toString();
+		logger.debug("Returning response: " + finalResponse);
+		return finalResponse;
 	}
 }
